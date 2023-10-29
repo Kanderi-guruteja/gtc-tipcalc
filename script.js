708e264f1af55e2f3e6ad503eb353c43b259cdd0
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-10-29 11:41:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const tipAmountInput = document.getElementById("tip-amount");
     const totalWithTipInput = document.getElementById("total-with-tip");
 
-    // Add an input event listener to the Bill Total input field
+    // Add input event listeners to the Bill Total and Tip input fields
     billTotalInput.addEventListener("input", calculateTip);
+    tipInput.addEventListener("input", calculateTip);
 
     // Function to calculate the tip and update the results
     function calculateTip() {
         const billTotal = parseFloat(billTotalInput.value);
+        const tipPercentage = parseFloat(tipInput.value);
         
-        if (isNaN(billTotal)) {
-            billTotalInput.setCustomValidity("Please enter a valid number");
+        if (isNaN(billTotal) || isNaN(tipPercentage)) {
+            billTotalInput.setCustomValidity("Please enter valid numbers");
             tipAmountInput.value = "0.00"; // Set Tip Amount to 0
             totalWithTipInput.value = "0.00"; // Set Total Bill with Tip to 0
         } else {
             billTotalInput.setCustomValidity(""); // Clear any previous error
-            const tipPercentage = tipInput.value;
             const tipAmount = (billTotal * tipPercentage) / 100;
             const totalWithTip = billTotal + tipAmount;
             tipPercentageDisplay.textContent = tipPercentage + "%";
             tipAmountInput.value = tipAmount.toFixed(2);
-            totalWithTipInput
+            totalWithTipInput.value = totalWithTip.toFixed(2);
+        }
+    }
+
+    // Initial calculation
+    calculateTip();
+});
