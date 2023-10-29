cfe3a75c6a5f05b0ae17055b834304ade1bbaf6a
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-10-29 08:31:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const billTotal = document.getElementById("bill-total");
-
-billTotal.addEventListener("input", () => {
-    const billAmount = parseFloat(billTotal.value);
-    if (isNaN(billAmount)) {
-        alert("Please enter a valid number for the Bill Total.");
-        billTotal.value = "";
-    }
-});
-
-const tipCalculatorForm = document.getElementById("tip-calculator-form");
 const tipRange = document.getElementById("tip");
 const tipAmount = document.getElementById("tip-amount");
 const totalWithTip = document.getElementById("total-with-tip");
+const tipPercentageDisplay = document.getElementById("tip-percentage-display");
 
-tipCalculatorForm.addEventListener("input", () => {
+tipRange.addEventListener("input", () => {
+    tipPercentageDisplay.innerText = tipRange.value + "%";
+});
+
+const calculateTip = () => {
     const billAmount = parseFloat(billTotal.value);
     const tipPercentage = parseFloat(tipRange.value);
+
     if (!isNaN(billAmount)) {
         const tip = (billAmount * tipPercentage) / 100;
         const total = billAmount + tip;
         tipAmount.value = tip.toFixed(2);
         totalWithTip.value = total.toFixed(2);
     }
-});
+};
+
+billTotal.addEventListener("input", calculateTip);
+tipRange.addEventListener("input", calculateTip);
