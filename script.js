add0bfd0ae438e92af25886c7c011994a6b4ee0a
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-10-29 20:53:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,27 +5,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const tipAmountInput = document.getElementById("tip-amount");
     const totalWithTipInput = document.getElementById("total-with-tip");
 
-    billTotalInput.addEventListener("input", calculateTip);
-    tipInput.addEventListener("input", calculateTip);
-
     function calculateTip() {
         const billTotal = parseFloat(billTotalInput.value);
         const tipPercentage = tipInput.value;
 
-        if (isNaN(billTotal) || isNaN(tipPercentage) || billTotalInput.validity.patternMismatch) {
-            alert("Please enter a valid number.");
-            billTotalInput.value = "";
-            tipAmountInput.value = "0.00";
-            totalWithTipInput.value = "0.00";
-        } else {
-            billTotalInput.setCustomValidity("");
+        if (!isNaN(billTotal) && !isNaN(tipPercentage)) {
             const tipAmount = (billTotal * tipPercentage) / 100;
             const totalWithTip = billTotal + tipAmount;
-            tipPercentageDisplay.textContent = tipPercentage + "%";
             tipAmountInput.value = tipAmount.toFixed(2);
             totalWithTipInput.value = totalWithTip.toFixed(2);
         }
     }
+
+    billTotalInput.addEventListener("input", calculateTip);
+    tipInput.addEventListener("input", calculateTip);
 
     calculateTip();
 });
