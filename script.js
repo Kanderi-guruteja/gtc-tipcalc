e2ffaec60013c94b643c112788ccd8bbcc07e81f
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-10-30 00:02:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// Function to calculate the tip and update the results
-function calculateTip() {
-  // Check if the bill total and tip percentage inputs contain any non-numeric characters
-  if (billTotalInput.value.match(/[^0-9.]/) || tipInput.value.match(/[^0-9.]/)) {
-    // Throw an error
-    throw new Error("Invalid input. Please enter numbers only.");
-  }
+document.addEventListener("DOMContentLoaded", function () {
+    const billTotalInput = document.getElementById("bill-total");
+    const tipInput = document.getElementById("tip");
+    const tipPercentageDisplay = document.getElementById("tip-percentage-display");
+    const tipAmountInput = document.getElementById("tip-amount");
+    const totalWithTipInput = document.getElementById("total-with-tip");
 
-  const billTotal = parseFloat(billTotalInput.value);
-  const tipPercentage = parseFloat(tipInput.value);
+    // Add input event listeners to the Bill Total and Tip input fields
+    billTotalInput.addEventListener("input", calculateTip);
+    tipInput.addEventListener("input", calculateTip);
 
-  if (isNaN(billTotal) || isNaN(tipPercentage)) {
-    billTotalInput.setCustomValidity("Please enter valid numbers");
-    tipAmountInput.value = "0.00"; // Set Tip Amount to 0
-    totalWithTipInput.value = "0.00"; // Set Total Bill with Tip to 0
-  } else {
-    billTotalInput.setCustomValidity(""); // Clear any previous error
-    const tipAmount = (billTotal * tipPercentage) / 100;
-    const totalWithTip = billTotal + tipAmount;
-    tipPercentageDisplay.textContent = tipPercentage + "%";
-    tipAmountInput.value = tipAmount.toFixed(2);
-    totalWithTipInput.value = totalWithTip.toFixed(2);
-  }
-}
+    // Function to calculate the tip and update the results
+    function calculateTip() {
+        const billTotal = parseFloat(billTotalInput.value);
+        const tipPercentage = parseFloat(tipInput.value);
 
-// Try/catch block to catch any errors
-try {
-  // Calculate the tip
-  calculateTip();
-} catch (error) {
-  // Display an error message to the user
-  alert("Error: " + error.message);
-}
+        if (isNaN(billTotal) || isNaN(tipPercentage)) {
+            billTotalInput.setCustomValidity("Please enter valid numbers");
+            tipAmountInput.value = "0.00"; // Set Tip Amount to 0
+            totalWithTipInput.value = "0.00"; // Set Total Bill with Tip to 0
+        } else {
+            billTotalInput.setCustomValidity(""); // Clear any previous error
+            const tipAmount = (billTotal * tipPercentage) / 100;
+            const totalWithTip = billTotal + tipAmount;
+            tipPercentageDisplay.textContent = tipPercentage + "%";
+            tipAmountInput.value = tipAmount.toFixed(2);
+            totalWithTipInput.value = totalWithTip.toFixed(2);
+        }
+    }
+
+    // Initial calculation
+    calculateTip();
+});
