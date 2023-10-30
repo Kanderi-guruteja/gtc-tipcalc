b75e8a7a7eade7b3c8b5cccda437e4b8f67abffe
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-10-29 20:43:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,33 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const tipAmountInput = document.getElementById("tip-amount");
     const totalWithTipInput = document.getElementById("total-with-tip");
 
-    // Add input event listeners to the Bill Total and Tip input fields
     billTotalInput.addEventListener("input", calculateTip);
-    tipInput.addEventListener("input", calculateTip);
 
-    // Function to calculate the tip and update the results
     function calculateTip() {
-        const billTotal = billTotalInput.value;
+        const billTotal = parseFloat(billTotalInput.value);
         const tipPercentage = tipInput.value;
-        
-        if (!isNumeric(billTotal) || !isNumeric(tipPercentage)) {
-            billTotalInput.setCustomValidity("Please enter valid numbers.");
+
+        if (isNaN(billTotal) || isNaN(tipPercentage) || billTotalInput.validity.patternMismatch) {
+            alert("Please enter a valid number.");
+            billTotalInput.value = ""; // Clear the input field
             tipAmountInput.value = "0.00"; // Set Tip Amount to 0
             totalWithTipInput.value = "0.00"; // Set Total Bill with Tip to 0
         } else {
-            billTotalInput.setCustomValidity(""); // Clear any previous error
+            billTotalInput.setCustomValidity("");
             const tipAmount = (billTotal * tipPercentage) / 100;
-            const totalWithTip = parseFloat(billTotal) + tipAmount;
+            const totalWithTip = billTotal + tipAmount;
             tipPercentageDisplay.textContent = tipPercentage + "%";
             tipAmountInput.value = tipAmount.toFixed(2);
             totalWithTipInput.value = totalWithTip.toFixed(2);
         }
     }
 
-    // Initial calculation
     calculateTip();
-
-    function isNumeric(value) {
-        return !isNaN(value) && isFinite(value);
-    }
 });
