64aaa7b9edd87b46dcef17c0beccbb2e4b6aa77b
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-10-30 00:40:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,25 +10,22 @@ document.addEventListener("DOMContentLoaded", function () {
     tipInput.addEventListener("input", calculateTip);
 
     // Function to calculate the tip and update the results
-    function calculateTip() {
-        const billTotal = billTotalInput.value;
-        const tipPercentage = parseFloat(tipInput.value);
-
-        if (!/^\d*\.?\d{0,2}$/.test(billTotal)) {
-            alert("Please enter a valid number.");
-            billTotalInput.setCustomValidity("Please enter a valid number");
-            tipAmountInput.value = "0.00"; // Set Tip Amount to 0
-            totalWithTipInput.value = "0.00"; // Set Total Bill with Tip to 0
-        } else {
-            billTotalInput.setCustomValidity(""); // Clear any previous error
-            const tipAmount = (billTotal * tipPercentage) / 100;
-            const totalWithTip = billTotal + tipAmount;
-            tipPercentageDisplay.textContent = tipPercentage + "%";
-            tipAmountInput.value = tipAmount.toFixed(2);
-            totalWithTipInput.value = totalWithTip.toFixed(2);
-        }
+   function calculateTip() {
+    const billTotal = parseFloat(billTotalInput.value);
+    const tipPercentage = parseFloat(tipInput.value);
+    
+    if (isNaN(billTotal) || isNaN(tipPercentage) || !/^\d*\.?\d{0,2}$/.test(billTotal)) {
+        alert("Please enter a valid number.");
+        billTotalInput.setCustomValidity("Please enter a valid number");
+        tipAmountInput.value = "0.00"; // Set Tip Amount to 0
+        totalWithTipInput.value = "0.00"; // Set Total Bill with Tip to 0
+    } else {
+        billTotalInput.setCustomValidity(""); // Clear any previous error
+        const tipAmount = (billTotal * tipPercentage) / 100;
+        const totalWithTip = billTotal + tipAmount;
+        tipPercentageDisplay.textContent = tipPercentage + "%";
+        tipAmountInput.value = tipAmount.toFixed(2);
+        totalWithTipInput.value = (billTotal + tipAmount).toFixed(2);
     }
+}
 
-    // Initial calculation
-    calculateTip();
-});
